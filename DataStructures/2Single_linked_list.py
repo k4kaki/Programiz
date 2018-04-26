6a74158cb21026df9d0b740f64717cff025db9a7
Your Name <you@example.com>
2018-04-26 07:34:54 +0000

    Yet another DS with linked list

---
+++ DataStructures/2Single_linked_list.py
@@ -0,0 +1,22 @@
+class Node:
+	def __init__(self,dataval=None):
+		self.dataval=dataval
+		self.nextval=None
+
+class SLinkedList:
+	def __init__(self):
+		self.headval=None
+	def listPrint(self):
+		printval=self.headval
+		while printval is not None:
+			print(printval.dataval)
+			printval=printval.nextval
+
+listO=SLinkedList()
+listO.headval=Node('Lun')
+e2=Node('Mar')
+e3=Node('Mie')
+listO.headval.nextval=e2
+e2.nextval=e3
+listO.listPrint()		
+		
